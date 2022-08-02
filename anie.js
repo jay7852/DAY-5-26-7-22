d5de6e1a9a2553d9f66b0b69d9bd09cd984587ec
jay7852 <105473456+jay7852@users.noreply.github.com>
2022-08-02 10:43:52 +0530

    Add files via upload

---
+++ anie.js
@@ -0,0 +1,6 @@
+const anime = ["Naruto", "My Hero academia ", "Vin land saga"];
+for (a of anime) {
+  document.write("<br/>",a);
+}
+
+console.log("by jay");
---
+++ name.js
@@ -0,0 +1,10 @@
+const inshan = {FirstName:"Jay", LastName:"patel", age:19};
+for (let A in inshan) {
+    document.write("<br/>",A);
+  }
+document.write("<hr/>");
+for (let A in inshan) {
+  document.write("<br/>",inshan[A]);
+}
+
+console.log("by jY");
